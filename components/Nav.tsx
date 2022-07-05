0fa9a993d1e53bfd9c088b44fee1046e1e6da348
Cayden Batten <clearlyrubix@gmail.com>
2022-07-05 11:38:34 -0600

    update

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
-import { ConnectButton } from 'web3uikit'
+
 const Nav = () => {
   return (
 	<nav>
-		<ul className='flex'>
+		<ul className='flex items-center justify-center'>
 			<li className='my-1 mx-2'>
 				<Link href='/'>Home</Link>
 			</li>
@@ -15,7 +15,7 @@ const Nav = () => {
 				<Link href='/proposals'>Proposals</Link>
 			</li>
 			<li className='my-1 mx-2'>
-				<ConnectButton />
+				Connect
 			</li>
 		</ul>
 	</nav>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 import type { AppProps } from 'next/app'
-import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MoralisProvider initializeOnMount={false}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </MoralisProvider>
-    
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
