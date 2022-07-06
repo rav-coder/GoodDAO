5bd5c2a69c5fd8d88cdb73a45d34a78466e01a05
Cayden Batten <clearlyrubix@gmail.com>
2022-07-06 15:47:36 -0600

    gray, purple, green colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'green': '#10b981',
+      'purple': '#8b5cf6',
+      'gray': '#e4e4e7'
+    },
     extend: {},
   },
   plugins: [],
