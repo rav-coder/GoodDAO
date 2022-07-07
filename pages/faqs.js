469c59c326650147d8216e8fc1c1e1b08a42add3
allanx1ing <allanxing2003@gmail.com>
2022-07-07 16:31:41 -0600

    added faqs and proposal page

---
+++ pages/faqs.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+import React from 'react'
+import Paragraph from '../components/Paragraph'
+
+const paragraphText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed porttitor lorem. Proin non est dictum, tempor lacus at, volutpat mauris. Phasellus consectetur eros erat, nec scelerisque massa elementum in. Sed sagittis lorem at lectus fermentum, id convallis tortor efficitur. Vivamus sollicitudin bibendum odio, quis ornare velit sodales congue. Curabitur auctor sed nunc id porta. Aenean nulla est, rutrum a orci nec, finibus rhoncus ex.'
+
+export default function Faqs() {
+    return (
+        <div className={styles.body}>
+            <Paragraph header="FAQ 1" text={paragraphText} />
+        </div>
+    )
+}
