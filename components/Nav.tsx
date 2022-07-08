46c1ad534221347e647108eed9d10442944dbc41
Cayden Batten <clearlyrubix@gmail.com>
2022-07-08 14:18:28 -0600

    move connect to right

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,15 +8,18 @@ const Nav = () => {
 
 			<nav className='h-24 w-screen p-3 bg-gradient-to-br from-sky-600 to-violet-900 text-white font-bold flex items-center justify-center fixed'>
 
-				<ul>
+				<ul className='flex flex-1 justify-center'>
 					<NavButton link={'/'} text={'Home'}/>
 					<NavButton link={'/stake'} text={'Stake'}/>
 					<NavButton link={'/proposals'} text={'Proposals'}/>
 					<NavButton link={'/faqs'} text={'FAQs'}/>
-					<Connect/>
+					
 				</ul>
+				<div className='flex flex-1 justify-end'>
+					<Connect/>
+				</div>
 			</nav>
-			<div className="hover:cursor-pointer fixed h-24 w-24 bg-[url('../public/logo.jpg')] bg-contain bg-no-repeat"></div>
+			<div className="hover:cursor-pointer fixed h-24 w-24 bg-[url('../public/logo.jpg')] bg-contain bg-no-repeat bg-white"></div>
 
 		</>
 	)
