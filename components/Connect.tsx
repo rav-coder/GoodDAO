04b639d2f4117dfac3260cf89c9be62d64368c4e
Saurav <76596911+rav-coder@users.noreply.github.com>
2022-07-15 13:14:16 -0600

    Price updates

--- components/Connect.tsx
+++ components/Connect.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import truncateWalletAddress from '../utils/truncateWalletAddress'
 
 const Connect = () => {
 
@@ -35,8 +36,7 @@ const Connect = () => {
 	if (isConnected && connector) {
 		return (
 			<div>
-				<div>{address}</div>
-				<div>Connected to {connector.name}</div>
+				<div>{truncateWalletAddress(address || 'N/A')}</div>
 				<button onClick={disconnectClick}>Disconnect</button>
 			</div>
 		)
