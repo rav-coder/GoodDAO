25b88a995d3ec2001411f2e7cf8059b2b88978e6
Cayden Batten <clearlyrubix@gmail.com>
2022-07-15 14:09:31 -0600

    move <a> outside div and add gitbook

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode, faHighlighter, faBook } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter, faDiscord, faGithub, faMedium, faInstagram, } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter, faDiscord, faGithub, faInstagram, } from '@fortawesome/free-brands-svg-icons'
 
 
 const Footer = () => {
@@ -22,33 +22,34 @@ const Footer = () => {
 
 		<div className='fixed bottom-0 justify-center w-screen'>
 			<div className="flex justify-between mx-auto pb-5 w-1/6 border-red-500 border-0">
-				<div className='flex justify-center align-middle items-center self-center bg-white w-10 h-10 rounded-full'>
-					<a href='https://twitter.com/BCharityFi' rel='noreferrer' target='_blank'>
+				<a href='https://twitter.com/BCharityFi' rel='noreferrer' target='_blank'>
+					<div className='flex justify-center align-middle items-center self-center bg-white w-10 h-10 rounded-full'>
 						<FontAwesomeIcon icon={faTwitter} />
-					</a>
-				</div>
+					</div>
+				</a>
 
-				<div className='flex justify-center align-middle items-center self-center bg-white w-10 h-10 rounded-full'>
-					<a href='https://discord.com/invite/4vKS59q5kV' rel='noreferrer' target='_blank'>
+				<a href='https://discord.com/invite/4vKS59q5kV' rel='noreferrer' target='_blank'>
+					<div className='flex justify-center align-middle items-center self-center bg-white w-10 h-10 rounded-full'>
 						<FontAwesomeIcon icon={faDiscord} />
-					</a>
-				</div>
+					</div>
+				</a>
 
-				<div className='flex justify-center align-middle items-center self-center bg-white w-10 h-10 rounded-full'>
-					<a href='https://www.instagram.com/bcharityofficial/' rel='noreferrer' target='_blank'>
+				<a href='https://www.instagram.com/bcharityofficial/' rel='noreferrer' target='_blank'>
+					<div className='flex justify-center align-middle items-center self-center bg-white w-10 h-10 rounded-full'>
 						<FontAwesomeIcon icon={faInstagram} />
-					</a>
-				</div>
-
-				<div className='flex justify-center align-middle items-center self-center bg-white w-10 h-10 rounded-full'>
-					<a href='https://github.com/ClearlyRubix/GoodDAO' rel='noreferrer' target='_blank'>
+					</div>
+				</a>
+				<a href='https://github.com/ClearlyRubix/GoodDAO' rel='noreferrer' target='_blank'>
+					<div className='flex justify-center align-middle items-center self-center bg-white w-10 h-10 rounded-full'>
 						<FontAwesomeIcon icon={faGithub} />
-					</a>
-				</div>
+					</div>
+				</a>
 
-				<div className='flex justify-center align-middle items-center self-center bg-white w-10 h-10 rounded-full'>
-					<FontAwesomeIcon icon={faMedium} />
-				</div>
+				<a href='https://clearlyrubix.gitbook.io/gooddao/' rel='noreferrer' target='_blank'>
+					<div className='flex justify-center align-middle items-center self-center bg-white w-10 h-10 rounded-full'>
+						<FontAwesomeIcon icon={faBook} />
+					</div>
+				</a>
 
 				<div className='absolute mr-6 right-0'>
 					<h1>Donate to our community wallet!</h1>
