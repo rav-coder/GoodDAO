17486fb57f82ce8f415c8f9bc6150987e931bf69
Cayden Batten <clearlyrubix@gmail.com>
2022-07-19 13:35:43 -0600

    delegation

---
+++ pages/delegateVote.tsx
@@ -0,0 +1,12 @@
+import DelegateBox from '../components/DelegateBox'
+
+export default function delegateVote() {
+
+
+    return (
+        <>
+            <h1 className='mt-[15vh] text-center text-4xl font-bold'>Delegate Vote</h1>
+            <DelegateBox/>
+        </>
+    )
+}
