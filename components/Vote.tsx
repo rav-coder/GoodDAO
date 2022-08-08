ec3a2bd3c69137af72abc27560eb3a98e8fafc8a
Cayden Batten <clearlyrubix@gmail.com>
2022-08-08 15:54:09 -0600

    better errors, styling, processing toasts

--- components/Vote.tsx
+++ components/Vote.tsx
@@ -1,5 +1,5 @@
 import { GOVERNANCE_ABI, GOVERNANCE_ADDRESS } from '../utils/constants'
-import { useContractRead, useContractReads, useContractWrite } from 'wagmi'
+import { useContractRead, useContractReads, useContractWrite, useWaitForTransaction } from 'wagmi'
 import { useState } from 'react';
 import {toast} from 'react-toastify'
 
@@ -21,6 +21,20 @@ export default function Vote({ id, vote }: Props) {
 			vote
 		],
 		onSuccess() {
+			toast.warn('Transaction Pending')
+		},
+		onError(error: any) {
+			if (error.error != null) {
+				toast.error(error.error.data.message)
+			} else {
+				toast.error(error.message)
+			}
+		},
+	})
+
+	const {isLoading} = useWaitForTransaction({
+		hash: castVote.data?.hash,
+		onSuccess() {
 			toast.success(vote == 0 ? 'Vote casted: Reject' : 'Vote casted: Approve')
 		},
 		onError(error: any) {
@@ -29,15 +43,23 @@ export default function Vote({ id, vote }: Props) {
 			} else {
 				toast.error(error.message)
 			}
-			
 		},
 	})
 
-	return (
-		<>
-			<button onClick={() => { castVote.write() }}>
-				{vote == 0 ? (<>Reject</>) : (<>Approve</>)}
-			</button>
-		</>
+
+
+	if (vote == 0) {
+		return (
+		<button onClick={() => { castVote.write() }} className='m-auto text-white bg-red-500 rounded-md py-2 px-3 mt-2 font-bold' disabled={isLoading}>
+			{isLoading? 'Processing...' : 'Reject'}
+		</button>
+		)
+	} 
+	else {
+		return (
+		<button onClick={() => { castVote.write() }} className='m-auto text-white bg-green rounded-md py-2 px-3 mt-2 font-bold' disabled={isLoading}>
+			{isLoading? 'Processing...' : 'Approve'}
+		</button>
 	)
+	}
 }
