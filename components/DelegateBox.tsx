dd43d0f228aced12bd92d440856468d89e21fd84
Cayden Batten <clearlyrubix@gmail.com>
2022-08-08 15:49:52 -0600

    note and better errors

--- components/DelegateBox.tsx
+++ components/DelegateBox.tsx
@@ -1,7 +1,7 @@
 import { formatUnits } from 'ethers/lib/utils'
 import React, { useState } from 'react'
 import { toast } from 'react-toastify'
-import { useContractReads, useContractWrite } from 'wagmi'
+import { useContractReads, useContractWrite, useWaitForTransaction } from 'wagmi'
 import { IGIVE_ABI, IGIVE_TOKEN } from '../utils/constants'
 
 
@@ -20,40 +20,30 @@ const DelegateBox = () => {
             address
         ],
 		onSuccess(data) {
-			toast.success('Delegated!')
+			toast.warn('Transaction Pending')
 		},
-		onError(error) {
-			toast.error(error.message)
-		}
+		onError(error: any) {
+			if (error.error != null) {
+				toast.error(error.error.data.message)
+			} else {
+				toast.error(error.message)
+			}
+		},
     })
 
-	// const stakers = useContractReads({
-	// 	contracts: [
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 0,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 1,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 2,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 3,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 4,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 5,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 6,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 7,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 8,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 9,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 10,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 11,]},	
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 12,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 13,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 14,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 15,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 16,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 17,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 18,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 19,]},
-	// 		{addressOrName: IGIVE_TOKEN, contractInterface: IGIVE_ABI, functionName: 'highestStakerInPool', args: [1, 20,]}
-	// 	]
-	// })
-
-	// console.log(formatUnits(stakers.data?.[7][0], 18))
+	const { isLoading: delegateLoading } = useWaitForTransaction({
+		hash: delegate.data?.hash,
+		onSuccess(data) {
+			toast.success('Delegated!')
+		},
+		onError(error: any) {
+			if (error.error != null) {
+				toast.error(error.error.data.message)
+			} else {
+				toast.error(error.message)
+			}
+		},
+	})
 
   return (
 	<div className='mt-[5vh] ml-[20vw] w-[60vw] h-[60vh] bg-white rounded-3xl border-purple border-4 p-6 overflow-auto font-bold'>
@@ -64,16 +54,19 @@ const DelegateBox = () => {
 	
 
 	Enter the Ethereum address of wallet to receive the voting rights.
-	<input className='w-full border-black rounded-lg border-2 p-1 font-bold' type={'text'} value ={address} onChange={(e)=>{setAddress(e.target.value)}} />
+	<input className='w-full border-black rounded-lg border-2 p-1 font-normal' type={'text'} value ={address} onChange={(e)=>{setAddress(e.target.value)}} />
 	<br/>
 	<br/>
-	<button onClick={()=>{delegate.write()}} className='bg-green rounded-2xl py-2 px-3 font-bold text-white ml-[50%] -translate-x-1/2'>
-		Delegate
+	<button onClick={()=>{delegate.write()}} className='bg-green rounded-2xl py-2 px-3 font-bold text-white ml-[50%] -translate-x-1/2' disabled={delegateLoading}>
+		{delegateLoading? 'Processing...' : 'Delegate'}
 	</button>
 	<br/>
 	<br/>
 	<p className='text-zinc-400 font-normal text-sm'>
 		By delegating your voting rights, you allow the recipient user to vote any decision on a proposal without your consent, however you can take back your rights by entering your address above and delegating back to yourself. The recipient does not take any ownership of your iGIVE tokens.
+	</p>
+	<p className='text-zinc-400 font-normal text-sm'>
+		**Please note: You cannot vote on proposals that were created before you delegated your vote. The transaction will go through, but your vote will not be counted.**
 	</p>
 	</div>
   )
